96769d364704de2e5c1a825ea8f9a97ce72d46e2
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-19 18:29:13 -0500

    adds workout table

--- models.py
+++ models.py
@@ -7,11 +7,28 @@ engine = create_engine('sqlite:///workout_data.db')
 
 Base = declarative_base()
 
+class Workout(Base):
+    __tablename__ = 'workouts'
+
+    id = Column(Integer, primary_key=True)
+    workout_name = Column(String)
+    difficulty = Column(String)
+    started_at = Column(DateTime(), server_default=func.now())
+    completed_at = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f'Workout(id={self.id}, ' + \
+            f'workout_name={self.name}, ' + \
+            f'difficulty={self.difficulty})'
+    
+
+
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    user_name = Column(String)
 
     def __repr__(self):
         return f'User(id={self.id}, ' + \
