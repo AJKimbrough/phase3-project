24ebecad96f67fe1ab922e5253db04231f2a8bda
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-19 18:12:13 -0500

    add engine

--- models.py
+++ models.py
@@ -3,4 +3,7 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine('sqlite:///workout_data.db')
+
 Base = declarative_base()
+
