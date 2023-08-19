2e8594d1fe453d3aaa8f99ff83de56386c71823f
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-19 18:51:43 -0500

    create session

--- seeds.py
+++ seeds.py
@@ -3,3 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 eninge = create_engine('sqlite:///workout_data.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
